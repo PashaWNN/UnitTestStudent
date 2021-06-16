cb01f9eb58a71b320c85147d8a4de5f8a002a60e
PashaWNN <pashawnn@gmail.com>
2021-06-17 00:34:29 +0900

    Fix migration; fix async running routine

--- uts/signals.py
+++ uts/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.db import transaction
 
 from uts.models import Solution
 from uts.tasks import run_solution
@@ -8,7 +9,7 @@ from uts.tasks import run_solution
 @receiver(post_save, sender=Solution)
 def _run_sandbox_callback(sender, instance: Solution, **__):
     if instance.state == Solution.NEW:
-        run_solution(instance.pk)
+        transaction.on_commit(lambda: run_solution.delay(instance.pk))
 
 
 ready = False
