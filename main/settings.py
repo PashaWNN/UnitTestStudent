06c37cbc7f36bfcbff6882af43e7ff7865f9150d
PashaWNN <pashawnn@gmail.com>
2021-06-17 16:09:21 +0900

    Configure postgres

--- main/settings.py
+++ main/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'database/db.sqlite3',
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.environ.get("SQL_DATABASE", "uts"),
+        "USER": os.environ.get("SQL_USER", "postgres"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "postgres"),
+        "HOST": os.environ.get("SQL_HOST", "postgres"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
